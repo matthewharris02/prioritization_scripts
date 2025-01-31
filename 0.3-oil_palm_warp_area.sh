bf2910e0255d3884105eadba11eb0386b6ca6838
Matthew Harris <matthewharris2002@hotmail.co.uk>
2025-01-31 14:00:59 +0000

    Fix errors in oil palm processing

--- 0.3-oil_palm_warp_area.sh
+++ 0.3-oil_palm_warp_area.sh
@@ -7,21 +7,28 @@
 dirBase='O:/f01_projects_active/Global/p09217_RestorationPotentialLayer/global2024_v2/raw/oil_palm'
 dirIn="${dirBase}/oil_palm_converted/intermediate_binary"
 res=5000
-dirOut="${dirBase}/oil_palm_converted/intermediate_${res}km"
+dirOut="${dirBase}/oil_palm_converted"
 
 while read -r line; do
     var="${line%.*}"
     
     oldName="${var}_converted.tif"
     newName_res="${var}_${res}km.tif"
-    newName_area="${var}_${res}km_${area}.tif"
-    mkdir -p $dirOut
+    newName_area="${var}_${res}km_area.tif"
+
+    dir_res="${dirOut}/intermediate_${res}km/"
+    mkdir -p $dir_res
 
     # Step 1: reduce resolution
-    gdalwarp -overwrite -s_srs EPSG:4326 -t_srs ESRI:54009 -tr $res $res -r sum "${dirIn}/${oldName}" "${dirOut}/${newName_res}"
+    gdalwarp -overwrite -s_srs EPSG:4326 -t_srs ESRI:54009 -tr $res $res -r sum -ot Float32 "${dirIn}/${oldName}" "${dir_res}/${newName_res}"
 
     # Step 2: convert sum to area
     scale_factor=$(( (res / 10)**2 ))
+    dir_area="${dirOut}/intermediate_${res}km_area/"
+    mkdir -p $dir_area
 
-    gdal_calc -A "${dirOut}/${newName_res}" --calc="A/${scale_factor}" --NoDataValue=-128 --outfile="${dirOut}/${newName_area}" --co compress=lzw --overwrite
+    gdal_calc -A "${dir_res}/${newName_res}" --calc="A/${scale_factor}" --NoDataValue=-128 --outfile="${dir_area}/${newName_area}" --co compress=lzw --overwrite
 done <"${dirBase}/files.txt"
+
+ls -l "${dir_area}/*.tif" > "${dir_area}/tiff_list.txt"
+gdal_merge -o "${dirOut}/full_oil_palm_5km_area.tif" --optfile "${dir_area}/tiff_list.txt"
