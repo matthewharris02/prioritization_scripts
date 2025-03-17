f5b650d6b927625adc5f2e6d9a19a159f31e8753
analysisuser2-Matthew <matthewharris2002@hotmail.co.uk>
2025-03-17 16:06:16 +0000

    Change = to <-

--- 1.1-OPTIONS.R
+++ 1.1-OPTIONS.R
@@ -4,24 +4,24 @@
 # RESOLUTION ====
 # Set shared resolution for all layers
 # Relative to 1km at equator (or 30 arcseconds in non-equal area projection)
-RES = 5
+RES <- 5
 
 # PROJECTION ====
 # Set EPSG to the EPSG code (e.g., EPSG:4326, ESRI:54009)
 # Set PROJ to the text that you want to label the files with (e.g., moll, laea)
-EPSG = "ESRI:54009" # EPSG code for the CRS
-PROJ = "moll" # Label ONLY
+EPSG <- "ESRI:54009" # EPSG code for the CRS
+PROJ <- "moll" # Label ONLY
 
 # EXTENT ====
 # This has been set to the maximum rounded extent within Mollweide (ESRI:54009)
 #       for resolutions up to 20 km
 # order: xmin, xmax, ymin, ymax; units: units of the projection
-EXT = c(-18040000, 18040000, -9020000, 9020000)
+EXT <- c(-18040000, 18040000, -9020000, 9020000)
 
 # HFP Range ====
 # the upper and lower values to set the range that we define 'intermediate' HFP
-hfp_lower = 3
-hfp_upper = 50 # Set to maximum so there is no upper
+hfp_lower <- 3
+hfp_upper <- 50 # Set to maximum so there is no upper
 
 
 # Directories ====
