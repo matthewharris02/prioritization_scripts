ed8a44d0e815956d3d579ccfc8ca1e104b12720a
Matthew Harris <matthewharris2002@hotmail.co.uk>
2025-04-22 11:41:18 +0100

    Cleaning: remove archives

--- ARCHIVES/Archive_other/ncp_manual_marshes_old.R
+++
@@ -1,9 +0,0 @@
-# Manually sort salt marshes from WCMC2017 version
-ncp_manual_marshes_old <- function(fn) {
-    marshes <- st_read(file.path(dir_in, fn)) |>
-        st_transform(st_crs(EPSG)) |>
-        rasterize(rast_template, cover = TRUE)
-    marshes_area <- marshes * (RES^2 * 1000^2) # manual area as we are using equal area
-    writeRaster(marshes_area, file.path(dir_out, "ncp", fn_template("ncp_saltmarshes")),
-                overwrite = TRUE)
-}
