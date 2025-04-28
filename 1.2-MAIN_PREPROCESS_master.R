61a488157da157519d783e99c3b3a615ae5efd50
analysisuser2-Matthew <matthewharris2002@hotmail.co.uk>
2025-04-28 13:44:56 +0100

    Update grid cell creation for large raster

--- 1.2-MAIN_PREPROCESS_master.R
+++ 1.2-MAIN_PREPROCESS_master.R
@@ -434,43 +434,94 @@ if (pp_ft_mask) {
 
 if (pp_cells) {
     print("* Creating grid cells *")
-    pu_rast <- c("countries", "lulc_converted", "lulc_other") |>
-        map(~file.path(dirs["dir_pu"], fn_template(.x))) |>
-        map(~rast(.x)) |>
+
+    # Prepare feature names and file paths
+    ft_names <- features |>
+        select(var) |>
+        deframe()
+    
+    ft_fns <- ft_names |>
+        sapply(\(x) fn_template(x, extra = "_mask")) |>
+        sapply(\(x) file.path(dirs["dir_ft"], x))
+
+    # Prepare names and file paths for other variables
+    other_names <- c("countries", "lulc_converted", "lulc_other", "restorable_land", "ecoregions") 
+
+    other_fns <- other_names |>
+        sapply(fn_template) |>
+        sapply(\(x) file.path(dirs["dir_pu"], x))
+    
+    # Create combined lists for names and file paths
+    all_names <- c(other_names, ft_names)
+    all_fns <- c(other_fns, ft_fns)
+ 
+    # Helper function to extract large raster with MakeTiles
+    large_extract <- function(rast, name, ntiles = 64) {
+        pritn(glue("Initializing large_extract() for {name} with {ntiles} tiles"))
+        # Set up directories
+        vals_dir <- file.path(dirs["dir_inter"], "temp_vals", name)
+        temp_fn <- file.path(dirs["dir_inter"], "temp_tiles", name, glue("temp_{name}_.tif"))
+        if (!dir.exists(dirname(temp_fn))) dir.create(dirname(temp_fn), recursive = TRUE)
+        if (!dir.exists(vals_dir)) dir.create(vals_dir, recursive = TRUE)
+        # Delete old files if exist
+        if( dir.exists(dirname(temp_fn))) {
+            existing <- list.files(dirname(temp_fn), full.names = TRUE)
+            lapply(existing, file.remove)
+        }
+
+        # Ensure ntiles is square number by taking the closest, lower square number
+        ntiles <- floor(ntiles ^ (1/2))^2
+
+        # Create template for MakeTiles
+        rast_tiles <- rast(
+            crs = crs(EPSG),
+            nrows = ntiles^(1/2),
+            ncols = ntiles^(1/2),
+            ext = ext(EXT)
+        )
+
+        print(glue("Making {ntiles} tiles for {name}..."))
+        tiles <- rast |>
+            makeTiles(rast_tiles, filename = temp_fn, overwrite = TRUE)
+        print("Tiles made")
+        for (i in seq(1, ntiles)) {
+            print(glue("Extracting for tile #{i}..."))
+            tile <- rast(tiles[i]) |>
+                as.data.frame(xy = TRUE, na.rm = NA) |>
+                write_parquet(file.path(vals_dir, glue("vals_{name}_{i}.parquet")))
+            rm(tile)
+        }
+        print("All tiles extracted; now loading values")
+        # Loading vals
+        vals_fns <- lapply(1:ntiles, \(x) file.path(vals_dir, glue("vals_{name}_{x}.parquet")))
+        vals <- rbindlist(lapply(vals_fns, read_parquet)) |>
+            write_parquet(file.path(dirs["dir_proc"], glue("{name}.parquet")))
+        print("Extraction complete!")
+    }
+
+    # Create multi-band raster for all variables
+    all_rast <- all_fns |>
+        sapply(rast) |>
         rast()
+    
+    # Set correct names for ISONUM and pu
+    all_names[1] <- "ISONUM"
+    all_names[4] <- "pu"
+    names(all_rast) <- all_names
 
-    pu <- rast(file.path(dirs["dir_pu"], fn_template("restorable_land")))
-    ecoregions <- rast(file.path(dirs["dir_pu"], fn_template("ecoregions")))
-
-    ft_present <- list.files(dirs["dir_ft"],
-                             pattern = "_mask.tif$",
-                             full.names = TRUE)
-    ft_names <- ft_present |>
-        basename() |>
-        str_remove_all(fixed(".tif")) |>
-        str_extract("(ft_[A-Za-z0-9]+)")
-    ft_rast <- ft_present |>
-        map(~rast(.x)) |>
-        rast()
-
-    all_rast <- c(pu_rast, pu, ecoregions, ft_rast)
-    names(all_rast) <- c("ISONUM", "lulc_converted", "lulc_other", "pu", "ecoregions", ft_names)
-    pu_vals <- as.data.frame(all_rast,
-                             xy = TRUE,
-                             na.rm = FALSE) |>
-        setDT()
-
-    pu_vals <- pu_vals[!is.na(ISONUM), ][!is.na(pu), ] # Filter our non 'restorable land'
-
-
-    pu_vals <- pu_vals[, id := 1:.N]
+    # Extract values
+    pu_vals <- large_extract(all_rast, "all")
+    
+    pu_vals <- pu_vals[!is.na(ISONUM),  # Ensure within UN boundary 
+                        ][!is.na(pu),   # Filter our non 'restorable land'
+                        ][, id := 1:.N] # Give unique id to each pu
+    
     setcolorder(pu_vals, "id", before = 1)
 
     write_dataset(pu_vals,
                   file.path(dirs["dir_proc"], "global_cells"),
                   partitioning = c("ISONUM"))
-
-
+    print("Grid cell creation complete")
 }
 
 end <- Sys.time()
