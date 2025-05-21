401869822d98ef7d85720bc93331060e1de4414d
analysisuser2-Matthew <matthewharris2002@hotmail.co.uk>
2025-05-21 13:24:38 +0100

    Update VRT processing for sentinel

--- 0.5-sentinel_snow.sh
+++ 0.5-sentinel_snow.sh
@@ -2,14 +2,15 @@
 #
 # ---------------------------------------------------------------------------- #
 dirBase='/mnt/sda/restoration_opportunities/raw/sentinel'
-var="convertedLand2020"
+var="gee_snow2020"
 dirOut=$dirBase/$var
 
 filesList=$dirOut/"files_${var}.txt"
 echo "Creating list of ${var} files: $filesList"
 rm -f $filesList
 find $dirOut -iname "*.tif" >> $filesList 
-outVrt=$dirOut/$var/"${var}_vrt.vrt"
+outVrt=$dirOut/"${var}_vrt.vrt"
 
 echo "Building Virtual Raster: $outVrt"
 gdalbuildvrt -input_file_list $filesList $outVrt
+gdal_translate $outVrt $dirBase/"${var}.tif" -co COMPRESS=LZW -co BIGTIFF=YES -co TILED=YES
